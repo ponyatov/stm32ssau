8109421664bc44a9aa3bd87614a03c6ebf4d9b6c
Dmitry Ponyatov <dponyatov@gmail.com>
2013-02-20 14:54:37 +0400

    + github adding scripts

--- gitconfig.bat
+++ gitconfig.bat
@@ -1,4 +1,21 @@
 git config --global user.email "dponyatov@gmail.com"
 git config --global user.name "Dmitry Ponyatov"
 pause
+exit
+
+rem Create a new repository on the command line
+
+touch README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin git@github.com:ponyatov/stm32ssau.git
+git push -u origin master
+
+rem Push an existing repository from the command line
+
+git remote add origin git@github.com:ponyatov/stm32ssau.git
+git push -u origin master
+
+
 
